48d546b55f32d46c8e7aba27bfaaba70bc553153
jiyeeeah <sije3757@snu.ac.kr>
2023-03-14 11:53:05 +0900

    구구단 파일 추가
    구구단 출력하는 파일을 추가

---
+++ index.js
@@ -0,0 +1,12 @@
+function hello() {
+  console.log("Hello World!");
+}
+function gugudan() {
+  for (let i = 1; i <= 9; i++) {
+    for (let j = 1; j <= 9; j++) {
+      console.log(i + " x " + j + " = " + i*j);
+    }
+  }
+}
+
+gugudan();
